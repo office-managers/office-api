1b8afe2e460b9472236e65b4cf911f4d074a1826
Chase <cabbott93@gmail.com>
2021-07-27 13:13:18 -0700

    add procfile

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,5 @@
 import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
-import request from 'supertest';
-import app from '../lib/app.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
