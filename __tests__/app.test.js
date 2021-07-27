89759f9caffea094bad1714a25d63c6e613ef7c8
Chase <cabbott93@gmail.com>
2021-07-27 11:47:50 -0700

    placeholder test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,13 @@
 import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
-import request from 'supertest';
-import app from '../lib/app.js';
+// import request from 'supertest';
+// import app from '../lib/app.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+  it('is a placeholder test', () => {
+
   });
 });
